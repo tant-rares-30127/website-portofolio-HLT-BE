7ada3c047fd261b03e8aebc7c89829206b4f76d2
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-09-22 23:15:24 +0300

    Add controller with getdata from login

---
+++ website-portofolio/website-portofolio/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using website_portofolio.Data;
+using website_portofolio.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace website_portofolio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LoginController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        // GET api/<LoginController>/5
+        [HttpGet]
+        public CVData Get([FromBody] Login login)
+        {
+            var loginsList = _context.Logins.ToList();
+            CVData chestie = null;
+            foreach (Login l in loginsList)
+            {
+                if (l.Username.Equals(login.Username) && l.Password.Equals(login.Password))
+                {
+                    chestie = _context.CVData.Include(n => n.ContactData).ToList()[login.Id];
+                    return chestie;
+                }
+            }
+            return chestie;
+            
+        }
+    }
+}
